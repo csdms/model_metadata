6472e2c9ee39ffe3fd1876148878906eb51f88c9
mcflugen <mcflugen@gmail.com>
2017-12-07 15:37:27 -0700

    Import from modelmetadata module.

--- model_metadata/__init__.py
+++ model_metadata/__init__.py
@@ -1,6 +1,6 @@
 from .model_parameter import ModelParameter
 from .model_info import ModelInfo
-from .model_metadata import ModelMetadata
+from .modelmetadata import ModelMetadata
 from .errors import MetadataNotFoundError
 
 __version__ = '0.1'
