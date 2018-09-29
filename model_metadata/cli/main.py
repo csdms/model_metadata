703ac138111dbd5cfa9ec6bc596d42694bd8c901
mcflugen <mcflugen@gmail.com>
2018-09-29 16:34:23 -0600

    Remove unused generate_parser.

--- model_metadata/cli/main.py
+++ model_metadata/cli/main.py
@@ -51,29 +51,7 @@ def configure_parser_mmd(sub_parsers=None):
     return p
 
 
-def generate_parser():
-    p = configure_parser_mmd()
-
-    # p = argparse.ArgumentParser(
-    #     description='Model metadata tools',
-    #     fromfile_prefix_chars='@',
-    # )
-
-    # sub_parsers = p.add_subparsers(
-    #     metavar='command',
-    #     dest='cmd',
-    # )
-    sub_parsers.required = True
-
-    configure_parser_mmd_find(sub_parsers)
-    configure_parser_mmd_stage(sub_parsers)
-    configure_parser_mmd_dump(sub_parsers)
-
-    return p
-
-
 def main():
-    # p = generate_parser()
     p = configure_parser_mmd()
     args = p.parse_args()
 
