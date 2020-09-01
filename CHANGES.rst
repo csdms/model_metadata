2114691a5c98dcaee0702eacce203db8905f462f
mcflugen <mcflugen@gmail.com>
2020-09-01 14:00:17 -0600

    add authors, changelog, convert readme to rst

---
+++ CHANGES.rst
@@ -0,0 +1,12 @@
+Changelog for model_metadata
+============================
+
+0.7.0 (unreleased)
+------------------
+
+- Removed versioneer; use zest.releaser instead
+
+
+0.5.0 (2019-05-15)
+------------------
+
--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 from setuptools import setup, find_packages
 
 
-def read_requirements():
-    import os
+def read(filename):
+    with open(filename, "r", encoding="utf-8") as fp:
+        return fp.read()
 
-    path = os.path.dirname(os.path.abspath(__file__))
-    requirements_file = os.path.join(path, "requirements.txt")
-    try:
-        with open(requirements_file, "r") as req_fp:
-            requires = req_fp.read().split()
-    except IOError:
-        return []
-    else:
-        return [require.split() for require in requires]
 
+long_description = u'\n\n'.join(
+    [
+        read('README.rst'),
+        read('AUTHORS.rst'),
+        read('CHANGES.rst'),
+    ]
+)
 
 setup(
     name="model_metadata",
     version="0.7.0.dev0",
     description="Model Metadata",
+    long_description=long_description,
     author="Eric Hutton",
     author_email="huttone@colorado.edu",
     url="http://csdms.colorado.edu",
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
@@ -33,16 +34,9 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Scientific/Engineering :: Physics",
     ],
+    keywords=["bmi", "pymt"],
     setup_requires=["setuptools"],
-    install_requires=[
-        "binaryornot",
-        "click",
-        "jinja2",
-        "packaging",
-        "pyyaml",
-        "py-scripting",
-        "six",
-    ],
+    install_requires=open("requirements.txt", "r").read().splitlines(),
     packages=find_packages(),
     entry_points={
         "console_scripts": ["mmd=model_metadata.cli.main:mmd"],
