d4dbcfa48d3755626acd927c52dca09a84fee984
mcflugen <mcflugen@gmail.com>
2024-01-21 21:28:43 -0700

    add a nox file

---
+++ noxfile.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import nox
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    """Look for lint."""
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files")
