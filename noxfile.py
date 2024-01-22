e65ef20f2c43f6eab0f71897fa55b11d60882f39
mcflugen <mcflugen@gmail.com>
2024-01-21 23:31:06 -0700

    add test session to nox file

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,28 @@
 from __future__ import annotations
 
+import os
+import pathlib
+
 import nox
+
+PROJECT = "model_metadata"
+ROOT = pathlib.Path(__file__).parent
+
+
+@nox.session
+def test(session: nox.Session) -> None:
+    """Run the tests."""
+    session.install("-r", "requirements.txt", "-r", "requirements-testing.txt")
+    session.install(".", "--no-deps")
+
+    args = ["--cov", PROJECT, "-vvv"] + session.posargs
+
+    if "CI" in os.environ:
+        args.append(f"--cov-report=xml:{ROOT.absolute()!s}/coverage.xml")
+    session.run("pytest", *args)
+
+    if "CI" not in os.environ:
+        session.run("coverage", "report", "--ignore-errors", "--show-missing")
 
 
 @nox.session
