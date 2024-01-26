c90d86fe9a454603017a2b36fc0779fe48e8a0e9
mcflugen <mcflugen@gmail.com>
2024-01-26 13:40:40 -0700

    remove install tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -5,7 +5,6 @@ import pathlib
 
 import pytest
 from model_metadata.api import find
-from model_metadata.api import install
 from model_metadata.api import query
 from model_metadata.api import stage
 from model_metadata.errors import MetadataNotFoundError
@@ -68,49 +67,6 @@ def test_stage(tmpdir, shared_datadir, as_type, dest):
     with tmpdir.as_cwd():
         manifest = stage(as_type(shared_datadir), dest)
         assert set(manifest) == set(os.listdir(dest))
-
-
-def test_install(tmpdir, shared_datadir):
-    with tmpdir.as_cwd():
-        install(str(shared_datadir), "./dest")
-        installed_files = os.listdir("./dest")
-        installed_files.sort()
-        assert installed_files == [
-            "api.yaml",
-            "child.in",
-            "info.yaml",
-            "parameters.yaml",
-            "run.yaml",
-        ]
-        for fname in installed_files:
-            assert os.path.isfile(os.path.join("dest", fname))
-            assert not os.path.islink(os.path.join("dest", fname))
-
-
-def test_install_develop(tmpdir, shared_datadir):
-    with tmpdir.as_cwd():
-        install(str(shared_datadir), "./dest", develop=True)
-        installed_files = os.listdir("./dest")
-        installed_files.sort()
-        assert installed_files == [
-            "api.yaml",
-            "child.in",
-            "info.yaml",
-            "parameters.yaml",
-            "run.yaml",
-        ]
-        for fname in installed_files:
-            assert os.path.islink(os.path.join("dest", fname))
-
-
-def test_install_clobber(tmpdir, shared_datadir):
-    with tmpdir.as_cwd():
-        os.mkdir("./dest")
-        with open(os.path.join("dest", "child.in"), "w") as fp:
-            fp.write("")
-        with pytest.raises(OSError):
-            install(str(shared_datadir), "./dest", clobber=False)
-        install(str(shared_datadir), "./dest", clobber=True)
 
 
 def test_stage_with_default_dest(tmpdir, shared_datadir):
